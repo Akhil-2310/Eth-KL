d191f129de4f0e8c77a6473263b2fee3a777c098
Akhil-2310 <akhilnanavati23@gmail.com>
2023-10-15 07:42:39 +0530

    frontend for the app

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import aleoLogo from "./assets/aleo.svg";
+import "./App.css";
+import votingsystem_program from "../voting_system/build/main.aleo?raw";
+import { AleoWorker } from "./workers/AleoWorker.js";
+
+const aleoWorker = AleoWorker();
+function App() {
+  const [count, setCount] = useState(0);
+  const [account, setAccount] = useState(null);
+  const [executing, setExecuting] = useState(false);
+  const [deploying, setDeploying] = useState(false);
+
+  const generateAccount = async () => {
+    const key = await aleoWorker.getPrivateKey();
+    setAccount(await key.to_string());
+  };
+
+  async function execute() {
+    setExecuting(true);
+    const result = await aleoWorker.localProgramExecution(votingsystem_program);
+    setExecuting(false);
+
+    alert(JSON.stringify(result));
+  }
+
+  async function deploy() {
+    setDeploying(true);
+    try {
+      const result = await aleoWorker.deployProgram(helloworld_program);
+      console.log("Transaction:");
+      console.log("https://explorer.hamp.app/transaction?id=" + result);
+      alert("Transaction ID: " + result);
+    } catch (e) {
+      console.log(e);
+      alert("Error with deployment, please check console for details");
+    }
+    setDeploying(false);
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://aleo.org" target="_blank">
+          <img src={aleoLogo} className="logo" alt="Aleo logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vote but anonymously</h1>
+      <div className="card">
+        <p>
+          <button onClick={generateAccount}>
+            {account
+              ? `Account is ${JSON.stringify(account)}`
+              : `Click to generate account`}
+          </button>
+        </p>
+
+        <input
+          type="text"
+          placeholder="Type here"
+          className="input input-bordered w-full max-w-xs"
+        />
+        <p>
+          <button disabled={executing} onClick={execute}>
+            {executing
+              ? `Executing...check console for details...`
+              : `Execute voting_system.aleo`}
+          </button>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
